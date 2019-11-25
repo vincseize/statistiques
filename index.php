0574ff7fa19e92c52b9a25fcf1c7ace33dc4248a
vincseize <vincseize@gmail.com>
2019-11-25 11:54:15 +0100

    Css et VAR_VIDER

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
-<?php
-include('inc/insert_donnees.php');
+<style>
+    body,html {
+        margin:0;
+        padding:0;
+        font-family: Arial;
+    }
+    .resultats_database{
+        display: block;
+        background-color: yellow;
+        font-size: 10px;
+    }
+</style>
+
+<div class="resultats_database">
+    <?php
+        include('inc/insert_donnees.php');
+    ?>
+</div>
+<br>
+
+<form>
+<input type="button" value="NOUVEAUX ELEVES ALEATOIRES (avec ecoles/sports)" onclick="window.location.href='index.php'" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="button" value="RESET" onclick="window.location.href='index.php?VAR_VIDER'" />
+</form> 
 
 
-?>
+<div class="resultats"></div>
